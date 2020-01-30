cca4edf955709cd50c46c1a6f5e583df592114d4
Juan Sebastian Montealegre <jusemonca@gmail.com>
2020-01-30 16:07:13 +0000

    [Feature]: Correct some raise errors

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,13 +3,13 @@
 Description of each state and the responses
 """
 
-from flask import jsonify, abort, request, make_response
 from api.v1.views import app_views
+from flask import jsonify, abort, request, make_response
 from models import storage
 from models.state import State
 
 
-@app_views.route("/states", strict_slashes=False)
+@app_views.route("/states", methods=['GET'], strict_slashes=False)
 def list_states():
     """Return a JSON list of states"""
     states = storage.all("State")
@@ -19,7 +19,7 @@ def list_states():
     return jsonify(list_state)
 
 
-@app_views.route("/states/<state_id>")
+@app_views.route("/states/<state_id>", methods=['GET'])
 def states_for_id(state_id):
     """Return a JSON list of states"""
     state = storage.get("State", state_id)
@@ -37,18 +37,18 @@ def delete_state(state_id):
     state.delete()
     storage.save()
 
-    return jsonify({})
+    return jsonify({}), 200
 
 
-@app_views.route("/states/", methods=['POST'])
+@app_views.route("/states/", methods=['POST'], strict_slashes=False)
 def post_state():
     """Return a JSON list of states"""
     json = request.get_json()
-    if not json:
-        return make_response(jsonify({"Not a JSON"}), 400)
+    if json is None:
+        abort(400, {'Not a JSON'})
     if 'name' not in json:
-        return make_response(jsonify({"Misssing name"}), 400)
-    new_state = State(**json)
+        abort(400, {'Missing name'})
+    new_state = State(name=json['name'])
     storage.new(new_state)
     storage.save()
 
@@ -60,13 +60,14 @@ def update_state(state_id):
     """Return a JSON list of states"""
     json = request.get_json()
     if not json:
-        return make_response(jsonify({"Not a JSON"}), 400)
+        abort(400, {'Not a JSON'})
     state = storage.get("State", state_id)
     if state is None:
         abort(404)
     new_dict = state.to_dict()
     for k, v in json.items():
-        if k == 'id' or k == 'created_at' or k == 'update_ati':
+        print("ESTO ES K ", k)
+        if k == 'id' or k == 'created_at' or k == 'updated_at':
             pass
         else:
             setattr(state, k, v)
